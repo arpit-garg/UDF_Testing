eb3835825dba44e9c284de633276e76414cba419
Arpit Garg <arpitgarg@Arpits-MacBook-Pro.local>
2015-07-14 20:45:43 +0530

    counting matches part1

--- test.py
+++ test.py
@@ -5,33 +5,32 @@ import re
 
 
 def test():
-	df = pandas.read_csv("../sample_data_set/Sheet1.csv")
-	#print df
+	df = pandas.read_csv("../sample_data_set.csv")
+	#print len(df)
 	df1 = df.dropna(axis = 1)
 	y = Series(df['CAID'].unique())
 	
 	#print y[345]
-
+	total = 0
 	leng = len(y)
 	#print leng
 	
 	df2 = DataFrame
-	
-	for i in range(0,1):
+	df3 = DataFrame()
+	for i in range(0,3):
 		
-		#df2.append(df1[df1['CAID'] == y[i]], ignore_index=True)
 		df2 = df1[df1['CAID'] == y[i]]
-		
-		print df2
+		df3 = df3.append(df2, ignore_index=False)
 		x = df2.index
-		print x
+		#print x
 		
 		data_len = len(df2)
-		print data_len
+		#print data_len
 		
-	#df2.to_csv("output.csv")
+		#df3.to_csv("output.csv")
+		#print len(df3)
 		
-		"""
+		"""	
 		first_index = x[0]
 		first_name = df1['Name'][x[2]]
 		print first_name
@@ -46,32 +45,35 @@ def test():
 			#result = re.split("\\W+|\\s+|\\d+", df2.loc[x1, 'Name'])
 			result.append(re.split("\\W+|\\s+|\\d+", df2.loc[x1, 'Name']))
 			
-			count = 1 #By default each Merchant name occurs 1 time
 		
-		for i in range(0, len(result)-1):
+		
+		j = 0
+		sum = 0
+		
+		k = 0
+		while k < len(result)-1:
+			count = 1 #By default each Merchant name occurs 1 time
+			print "CAID: " + str(df2['CAID'][x[k]]) + "," + "result: " + str(result[k])
+			#print len(result[k])
 			
-			#print result[i]
-			#print len(result[i])
+			if result[k][0] == result[k+1][0]:
+				count += 1
+				for j in range(k+1, len(result)-1):
+					if result[j][0] == result[j+1][0]:
+						count += 1
+					else:
+						break
+				k += count
+			else:
+				count = 1
+				k += 1
 			
-			#comparing each split within
-			for j,k in zip((0, len(result[i])), (0, len(result[i+1]))):	
+			sum += count
+			print "count: "+ str(count)
+		total += sum
+		print total
 			
-				if result[i][j] == result[i+1][k]:
-					try:
-					
-						print result[i][j] + "==" + result[i+1][k]
-					except e:
-						result[i][j] + "==" + result[i+1][k]
-
-
-				
-				
-			#result_df = DataFrame(result)
-		#print result_df
-#def patt_matching():
 	
-	
-
 
 
 if __name__ == "__main__":
@@ -81,9 +83,8 @@ if __name__ == "__main__":
 
 """
 1.Make separate functions for all the loopings
-2.check for if conditions within dataframes.
-3.return count, caid and merchant_name for every matched pattern 
-  and then store it in a bigger array
-4.check a method for pattern matching.
+2.append to a dataframe
+3.check a method for pattern matching.(data matching techniques)
+
 """
 
